fcd28925dd09c12916de37dcdfebb3d4d795f717
nect2r <nect2r@kakao.com>
2021-10-12 20:04:48 +0900

    JPA의 영속성 컨텍스트에 대한 기본이해
    비영속 , 영속 , 준영속(분리) , 삭제

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -18,16 +18,23 @@ public class JpaMain {
 
         try{
 
-            //hellojpa.Member findMember = em.find(hellojpa.Member.class, 1L);
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(1)
-                    .setMaxResults(10)
-                    .getResultList();
+            //비영속
+            Member member = new Member();
+            member.setId(100L);
+            member.setName("HelloJPA");
 
-            for (Member member : result) {
-                System.out.println("hellojpa.Member.name " + member.getName());
-            }
+            //영속 -> 영속성 컨텍스트에서 관리
+            System.out.println("=== BEFORE ===");
+            em.persist(member);
+            System.out.println("=== AFTER ===");
 
+            //영속성 컨테스트에 들어있는 멤버 객체를 컨텍스트에서 분리 , 준영속 상태
+            em.detach(member);
+
+            //실제 DB의 객체를 삭제하는상태태
+           em.remove(member);
+
+            //커밋시 -> 영속성 컨텍스트에서 관리중인 객체를 DB로 이동
             tx.commit();
         } catch (Exception e){
             tx.rollback();
