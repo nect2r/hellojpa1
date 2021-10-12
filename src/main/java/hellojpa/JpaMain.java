f58e5f406ef86ca31d1e786a5dd3bc19ba2432d9
nect2r <nect2r@kakao.com>
2021-10-12 20:32:38 +0900

    영속성 컨텍스트의 이점
    1차 캐시 , 동일성 보장(같은 엔티티매니지안에서)
    트랜잭션을 지원하는 쓰기 지연 (SQL 쿼리를 담아서 보냄)
    변경 감지
    지연 로딩
    
    들의 기본적인 이해

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -17,24 +17,9 @@ public class JpaMain {
         tx.begin();
 
         try{
+            Member member = em.find(Member.class, 150L);
+            member.setName("ZZZZZ");
 
-            //비영속
-            Member member = new Member();
-            member.setId(100L);
-            member.setName("HelloJPA");
-
-            //영속 -> 영속성 컨텍스트에서 관리
-            System.out.println("=== BEFORE ===");
-            em.persist(member);
-            System.out.println("=== AFTER ===");
-
-            //영속성 컨테스트에 들어있는 멤버 객체를 컨텍스트에서 분리 , 준영속 상태
-            em.detach(member);
-
-            //실제 DB의 객체를 삭제하는상태태
-           em.remove(member);
-
-            //커밋시 -> 영속성 컨텍스트에서 관리중인 객체를 DB로 이동
             tx.commit();
         } catch (Exception e){
             tx.rollback();
